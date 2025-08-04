0ec24934c05b98bda67acf53b7d441d891598468
k2121 <31193029+k2121@users.noreply.github.com>
2025-08-04 10:36:32 +0200

    Create select2.Pola_Planszy.js

---
+++ select2.Pola_Planszy.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function () {
+  // Inicjalizacja select2 (jeśli jeszcze nie ma)
+  $('#pola_planszy').select2();
+
+  // Timeout pozwala Select2 wygenerować DOM
+  setTimeout(function () {
+    const container = $('#pola_planszy').next('.select2-container');
+    container.find('.select2-selection--single').css({
+      'height': '18px',
+      'line-height': '18px',
+      'padding': '0 4px',
+      'font-size': '10px'
+    });
+
+    container.find('.select2-selection__rendered').css({
+      'line-height': '18px',
+      'padding': '0 4px',
+      'font-size': '10px'
+    });
+
+    container.find('.select2-selection__arrow').css({
+      'height': '18px'
+    });
+  }, 0);
+});
