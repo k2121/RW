c6792e540f0881c4c3ee016aaa79c92fb2e6b16e
k2121 <31193029+k2121@users.noreply.github.com>
2025-08-08 10:45:14 +0200

    Update select2.Pola_Planszy.js

--- select2.Pola_Planszy.js
+++ select2.Pola_Planszy.js
@@ -15,3 +15,59 @@ $(document).ready(function () {
       'height:18px !important; top:0 !important;');
   }, 0);
 });
+
+
+$(document).ready(function () {
+  $('#pola_zdarzenie').select2();
+
+  setTimeout(function () {
+    const $container = $('#pola_zdarzenie').next('.select2-container');
+
+    // Dodaj style jako atrybut style z !important
+    $container.find('.select2-selection--single').attr('style',
+      'height:18px !important; line-height:18px !important; padding:0 0px !important;');
+
+    $container.find('.select2-selection__rendered').attr('style',
+      'line-height:18px !important; padding:0 0px !important;');
+
+    $container.find('.select2-selection__arrow').attr('style',
+      'height:18px !important; top:0 !important;');
+  }, 0);
+});
+
+$(document).ready(function () {
+  $('#pola_kontekst').select2();
+
+  setTimeout(function () {
+    const $container = $('#pola_kontekst').next('.select2-container');
+
+    // Dodaj style jako atrybut style z !important
+    $container.find('.select2-selection--single').attr('style',
+      'height:18px !important; line-height:18px !important; padding:0 0px !important;');
+
+    $container.find('.select2-selection__rendered').attr('style',
+      'line-height:18px !important; padding:0 0px !important;');
+
+    $container.find('.select2-selection__arrow').attr('style',
+      'height:18px !important; top:0 !important;');
+  }, 0);
+});
+
+$(document).ready(function () {
+  $('#karma').select2();
+
+  setTimeout(function () {
+    const $container = $('#karma').next('.select2-container');
+
+    // Dodaj style jako atrybut style z !important
+    $container.find('.select2-selection--single').attr('style',
+      'height:18px !important; line-height:18px !important; padding:0 0px !important;');
+
+    $container.find('.select2-selection__rendered').attr('style',
+      'line-height:18px !important; padding:0 0px !important;');
+
+    $container.find('.select2-selection__arrow').attr('style',
+      'height:18px !important; top:0 !important;');
+  }, 0);
+});
+
