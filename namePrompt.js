f70f27122e6c81a0251cfe3a8ac010ec1426d443
k2121 <31193029+k2121@users.noreply.github.com>
2025-08-08 17:17:19 +0200

    Update namePrompt.js

--- namePrompt.js
+++ namePrompt.js
@@ -1,15 +1,10 @@
-//#### 2. `js/namePrompt.js`
-//Zarządzanie nazwą użytkownika. Zmienna `userName` musi być dostępna globalnie.
-
-// js/namePrompt.js
-
 let userName = null; // Ta zmienna musi być dostępna globalnie
 
 function promptForName() {
   if (userName === null || userName.trim() === "") {
-    userName = prompt("Jak się mam do ciebie zwracać?");
+    userName = prompt("Jak się mam do ciebie zwracać?", "Krzysiu");
     if (!userName || userName.trim() === "") {
-      userName = null;
+      userName = "Krzysiu";
     }
   }
 }
