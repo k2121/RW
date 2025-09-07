5a40e238356d546e0bae10563b7ed9db415a2fb6
k2121 <31193029+k2121@users.noreply.github.com>
2025-09-07 09:33:54 +0200

    Add files via upload

---
+++ otworzCzakryFull.js
@@ -0,0 +1,19 @@
+  function otworzCzakryFull() {
+    const win = window.open('', 'Czakry Full', 'width=800,height=600');
+    win.document.write(`
+      <html>
+        <head>
+          <title>Czakry Full</title>
+          <style>
+            body { margin: 0; text-align: center; background: #000; }
+            img { max-width: 100%; height: auto; }
+            button { position: absolute; top: 10px; right: 10px; font-size: 60px; }
+          </style>
+        </head>
+        <body>
+          <button onclick="window.close()">✖</button>
+          <img src="Czakry_symbole_x7_v=MvOpK-FuAZY.jpg" alt="Czakry Full">
+        </body>
+      </html>
+    `);
+  }
