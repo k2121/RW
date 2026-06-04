4e9d6f29992aa5597e072ccffc826e3e9fd3ae39
k2121 <31193029+k2121@users.noreply.github.com>
2026-06-04 13:48:57 +0200

    kostka od nowej linii ok

--- gameMechanics.js
+++ gameMechanics.js
@@ -95,7 +95,7 @@ function rollDice() {
   const nextFieldData = sampleData.pola_planszy.find(p => parseInt(p.value) === nextPos);
   const nextLabel = nextFieldData ? nextFieldData.label : nextPos;
 
-  diceResultElement.innerText += `🎲 = ${diceResult} (${nextLabel})`;
+  diceResultElement.innerText = `<br>🎲 = ${diceResult} (${nextLabel})`;
 }
 
 function moveMinus16() {
@@ -113,7 +113,7 @@ function moveMinus16() {
   const nextFieldData = sampleData.pola_planszy.find(p => parseInt(p.value) === nextPos);
   const nextLabel = nextFieldData ? nextFieldData.label : nextPos;
 
-  diceResultElement.innerText += `⏪ = -16 (${nextLabel})`;
+  diceResultElement.innerText = `<br>⏪ = -16 (${nextLabel})`;
 }
 
 function initPolaPlanszyDropdown() {
