765e9847e44fd06340656731ec0b9be91f09f57c
root <to.ja.krzysio@gmail.com>
2026-07-22 15:39:04 +0200

    fotki Planszy

--- otworzPlansza.js
+++ otworzPlansza.js
@@ -50,8 +50,31 @@ function otworzPlansza() {
         <body>
           <button onclick="window.close()">✖</button>
           <div class="gallery">
-            <img src="Plansza.jpg" alt="Plansza">
-            <img src="karty satori.jpg" alt="Karty Satori">
+            <img src="Plansza.jpg" alt="Plansza" onclick="window.open(this.src, '_blank')">
+            <img src="karty satori.jpg" alt="Karty Satori" onclick="window.open(this.src, '_blank')">
+            <img src="001 Karty.jpeg" alt="001 Karty.jpeg" onclick="window.open(this.src, '_blank')">
+            <img src="002 Karty EN.jpeg" alt="002 Karty EN.jpeg" onclick="window.open(this.src, '_blank')">
+            <img src="003 Karty EN1.jpeg" alt="003 Karty EN1.jpeg" onclick="window.open(this.src, '_blank')">
+            <img src="004 Karty EN2.jpeg" alt="004 Karty EN2.jpeg" onclick="window.open(this.src, '_blank')">
+            <img src="005 Karty EN3.jpeg" alt="005 Karty EN3.jpeg" onclick="window.open(this.src, '_blank')">
+            <img src="006 Karty EN4.jpeg" alt="006 Karty EN4.jpeg" onclick="window.open(this.src, '_blank')">
+            <img src="007 Plansza cała.jpeg" alt="007 Plansza cała.jpeg" onclick="window.open(this.src, '_blank')">
+            <img src="008 Plansza EN.jpeg" alt="008 Plansza EN.jpeg" onclick="window.open(this.src, '_blank')">
+            <img src="009 Plansza EN2.jpeg" alt="009 Plansza EN2.jpeg" onclick="window.open(this.src, '_blank')">
+            <img src="010 Plansza EN3.jpeg" alt="010 Plansza EN3.jpeg" onclick="window.open(this.src, '_blank')">
+            <img src="011 Plansza z bliska.jpeg" alt="011 Plansza z bliska.jpeg" onclick="window.open(this.src, '_blank')">
+            <img src="012 Przewodnik gry.jpeg" alt="012 Przewodnik gry.jpeg" onclick="window.open(this.src, '_blank')">
+            <img src="013 Pudełko logo.jpeg" alt="013 Pudełko logo.jpeg" onclick="window.open(this.src, '_blank')">
+            <img src="014 Pudełko w środku.jpeg" alt="014 Pudełko w środku.jpeg" onclick="window.open(this.src, '_blank')">
+            <img src="015 Pudełko zniszczone.jpeg" alt="015 Pudełko zniszczone.jpeg" onclick="window.open(this.src, '_blank')">
+            <img src="016 Wszystko EN.jpeg" alt="016 Wszystko EN.jpeg" onclick="window.open(this.src, '_blank')">
+            <img src="017 Wszystko EN0.jpeg" alt="017 Wszystko EN0.jpeg" onclick="window.open(this.src, '_blank')">
+            <img src="018 Wszystko EN2.jpeg" alt="018 Wszystko EN2.jpeg" onclick="window.open(this.src, '_blank')">
+            <img src="019 Wszystko EN3.jpeg" alt="019 Wszystko EN3.jpeg" onclick="window.open(this.src, '_blank')">
+            <img src="020 Wszystko1.jpeg" alt="020 Wszystko1.jpeg" onclick="window.open(this.src, '_blank')">
+            			
+			<&ZNAK.UNICODE(34)&>
+			
           </div>
         </body>
       </html>
